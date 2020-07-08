4cd12040cad27691346e9b76b468a03629d43a97
gokmenozkn <kanjey159@gmail.com>
2020-07-08 17:01:20 +0300

    updated

--- src/store/firebase.js
+++ src/store/firebase.js
@@ -4,13 +4,13 @@ import 'firebase/firestore';
 require('dotenv').config();
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.BUCKET,
-  messagingSenderId: process.env.SENDER_ID,
-  appId: process.env.APP_ID
+  apiKey: `${process.env.API_KEY}`,
+  authDomain: `${process.env.AUTH_DOMAIN}`,
+  databaseURL: `${process.env.DATABASE_URL}`,
+  projectId: `${process.env.PROJECT_ID}`,
+  storageBucket: `${process.env.BUCKET}`,
+  messagingSenderId: `${process.env.SENDER_ID}`,
+  appId: `${process.env.APP_ID}`
 };
 
 if (!firebase.apps.length) {
